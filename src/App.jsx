1593da4f38608b68da49af906ee14f25872e39f4
iamfiham <xyzfiham@gmail.com>
2024-03-08 17:55:50 +0530

    Card component completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./Components/Card/Card";
+
+function App() {
+  return (
+    <section className="app">
+      <Card />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import "./assets/ComponentsStyle.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="main-border"></div>
+      <div className="content">
+        <div className="sub-border"></div>
+        <div className="content-bg"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
